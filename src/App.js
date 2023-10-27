691c42dd9956213058961d28cd17f3f2f7c12324
amaan8429 <mohdamaan8429@gmail.com>
2023-10-27 06:17:01 +0530

    created intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./styles.scss";
 
-function App() {
+function EmailLink() {
+  return <a href="mailto:amaanrizvi73@gmail.com">Mail</a>;
+}
+
+function LinktreeLink() {
+  return <a href="https://linktr.ee/amaan8429">Other Links</a>;
+}
+
+function Intro() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="intro">
+      <h3 className="intro__subtitle">
+        The goal isn't to live forever, the goal is to create something that
+        will.
+      </h3>
+      <h1 className="intro__title">
+        Official Website of the Compunerd Society of Goel Institute of
+        Technology and Management.
+      </h1>
+      <p className="intro__text">
+        If you are from GITM and want to collaborate on this project , please
+        get in touch with Us.
+      </p>
+      <p className="intro__subtitle">
+        Contact us using <EmailLink /> or <LinktreeLink />.
+      </p>
     </div>
   );
 }
 
-export default App;
+export default Intro;
